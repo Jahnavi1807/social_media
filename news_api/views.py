b59f46042363b7235882976614dcd2651dc6946d
JyothirmaiSuravarapu <jyothirmaisuravarapu@gmail.com>
2023-04-21 12:44:57 +0530

    modifying_news

--- news_api/views.py
+++ news_api/views.py
@@ -4,10 +4,21 @@ API_KEY = 'ab547a533c5c41659e5e03d2132abdc3'
 # Create your views here.
 
 def news(request):
-	url = f'https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey={API_KEY}'
-	response = requests.get(url)
-	data = response.json()
-	articles = data['articles']
+	country = request.GET.get('country')
+	category = request.GET.get('category')
+
+	if country:
+		
+		url = f'https://newsapi.org/v2/top-headlines?country={country}&apikey={API_KEY}'
+		response = requests.get(url)
+		data = response.json()
+		articles = data['articles']
+	else:
+		url = f'https://newsapi.org/v2/top-headlines?category={category}&apikey={API_KEY}'
+		response = requests.get(url)
+		data = response.json()
+		articles = data['articles']
+
 
 	context = {
 	'articles' : articles }
